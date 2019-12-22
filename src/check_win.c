4724847b09a35daad90f26740228c3800696487b
Florian Thibaud <florian.thibaud@epita.fr>
2019-12-22 23:17:21 +0100

    add the file for the check of win, not working well for all the possibilites

---
+++ src/check_win.c
@@ -0,0 +1,79 @@
+#include "check_win.h"
+#include <stdio.h>
+static int check_ldiag(char **board, char player, int line, int col)
+{
+    int win = 0;
+    for (; line >= 0 && col < 7; line--, col++)
+    {
+        if (win == 4)
+            return 1;
+        if (board[line][col] != player)
+            return 0;
+        else
+            win++;
+    }
+    return 0;
+}
+
+
+static int check_rdiag(char **board, char player, int line, int col)
+{
+    int win = 1;
+    for (; line < 6 && col < 7; line++, col++)
+    {
+        if (win == 4)
+            return 1;
+        if (board[line][col] != player)
+            return 0;
+        else
+            win++;
+    }
+    return 0;
+}
+
+
+static int check_line(char **board, char player, int line, int col)
+{
+    int win = 0;
+    for (; col < 7; col++)
+    {
+        if (win == 4)
+            return 1;
+        if (board[line][col] != player)
+            return 0;
+        else
+            win++;
+    }
+    return 0;
+}
+
+
+static int check_col(char **board, char player, int line, int col)
+{
+    int win = 1;
+    for (; line < 6; line++)
+    {
+        if (win == 4)
+            return 1;
+        if (board[line][col] != player)
+            return 0;
+        else
+            win++;
+    }
+    return 0;
+}
+
+int check_win(char **board, char player)
+{
+    for (int i = 0; i < 6; i++)
+    {
+        for (int j = 0; j < 7; j++)
+        {
+            if (check_ldiag(board, player, i, j) || check_rdiag(board, player, i, j))
+                return 1;
+            if (check_line(board, player, i, j) || check_col(board, player, i, j))
+                return 1;
+        }
+    }
+    return 0;
+}
---
+++ src/check_win.h
@@ -0,0 +1,15 @@
+#ifndef CHECK_WIN_H
+#define CHECK_WIN_H
+
+
+/**
+** \brief function that check if the player player has won the game
+**
+** \param board: where it's check
+** \param player
+**
+** \return 1 if the player has won, 0 othersise
+*/
+int check_win(char **board, char player);
+
+#endif /* !CHECK_WIN_H */
